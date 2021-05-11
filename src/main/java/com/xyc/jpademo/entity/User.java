073d2acc8b9dc1fb569062e3602e927826c73c73
xuyuchao <949001430@qq.com>
2021-05-11 22:25:04 +0800

    cmt

---
+++ src/main/java/com/xyc/jpademo/entity/User.java
@@ -0,0 +1,9 @@
+package com.xyc.jpademo.entity;
+
+/**
+ * Created by 1 on 2021/5/11.
+ */
+public class User
+{
+
+}
---
+++ src/test/java/com/xyc/TstjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TstjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
